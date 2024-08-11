a314f3d963980dcc4719adb33b39ca41aefd8420
ForcesR2N <muhammadzuhrizal07@gmail.com>
2024-08-11 23:02:03 +0700

    add selector

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+int _selectedGender = 0, _height = 170, _weight = 50, _age = 15;
+double _bmi = 0;
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +30,65 @@ class HomePage extends StatelessWidget {
 
   Widget _BuildUI() {
     return Column(
-      children: [],
+      children: [
+        _genderSelector(),
+      ],
+    );
+  }
+
+  Widget _genderSelector() {
+    return Container(
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _selectedGender = 0;
+                  });
+                },
+                iconSize: 70,
+                icon: Icon(
+                  Icons.male_rounded,
+                  color: _selectedGender == 0
+                      ? Theme.of(context).colorScheme.primary
+                      : Colors.black,
+                ),
+              ),
+              Text(
+                "Male",
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _selectedGender = 1;
+                  });
+                },
+                iconSize: 70,
+                icon: Icon(
+                  Icons.female_rounded,
+                  color: _selectedGender == 1
+                      ? Theme.of(context).colorScheme.primary
+                      : Colors.black,
+                ),
+              ),
+              Text(
+                "Female",
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
